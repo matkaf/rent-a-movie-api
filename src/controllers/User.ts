7da5f524f71916d2483d5eeb6f67d4fe72f00a80
Matheus Kaffka <matheus.kaffka@gmail.com>
2022-07-05 00:32:20 -0300

    feat: cria controller User

---
+++ src/controllers/User.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+
+import User from "../services/User";
+
+const findUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body
+
+  const { found, message } = await User.findUser(email, password)
+  
+  if (!found) return res.status(StatusCodes.BAD_REQUEST).json({ message })
+  
+  return res.status(StatusCodes.OK).json({ message })
+}
+
+export default {
+  findUser,
+}
