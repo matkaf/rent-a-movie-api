d3caf24b46a835d8a0fceb1f85c3f49d3edd16b7
Matheus Kaffka <matheus.kaffka@gmail.com>
2022-07-05 00:31:21 -0300

    feat: cria service User

---
+++ src/services/User.ts
@@ -0,0 +1,17 @@
+import User from "../models/User";
+
+const findUser = async (email: string, password: string) => {
+  const user = await User.findUser(email)
+
+  if (!user) return { found: false, message: 'E-mail não encontrado' }
+
+  const wrongPassword = user.password !== password;
+
+  if (wrongPassword) return { found: false, message: 'Senha incorreta' }
+
+  return { found: true, message:  user.name }
+}
+
+export default {
+  findUser,
+}
