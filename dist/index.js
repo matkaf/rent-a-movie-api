ff1f86eeded90cd11bc90b05109a4cc1976d2ece
Matheus Kaffka <matheus.kaffka@gmail.com>
2022-07-06 08:56:31 -0300

    chore: heroku

--- dist/index.js
+++ dist/index.js
@@ -12,13 +12,13 @@ const Movie_1 = __importDefault(require("./controllers/Movie"));
 const MovieRental_1 = __importDefault(require("./controllers/MovieRental"));
 const User_1 = __importDefault(require("./controllers/User"));
 const origin = ['http://localhost:3000'];
+const PORT = process.env.PORT || 3001;
 const options = {
     origin,
 };
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)(options));
 app.use(express_1.default.json());
-const port = 3001;
 app.get('/', (_req, res) => {
     res.status(http_status_codes_1.StatusCodes.OK).send('API funcionando!');
 });
@@ -27,6 +27,6 @@ app.get('/movies', Movie_1.default.getAllMovies);
 app.post('/movies', MovieRental_1.default.createMovieRental);
 app.get('/games', Game_1.default.getAllGames);
 app.post('/games', GameRental_1.default.createGameRental);
-app.listen(process.env.PORT || port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
